7350e2341f14076d40f2bb5ddb8e718f9219292d
Chen Weiguang <chen.weiguang@gmail.com>
2018-03-09 16:08:14 +0800

    Remove config reading

--- src/conf.rs
+++ src/conf.rs
@@ -3,11 +3,17 @@ use mega_coll::conf::{app, fluentd, hdfs, krb5};
 #[derive(StructOpt, Debug)]
 #[structopt(name = "rs-hdfs-report-conf",
             about = "Configuration for Rust hdfs-report")]
-pub struct ArgConf {
+pub struct ArgConfig {
     #[structopt(short = "c", long = "conf",
                 default_value = "config/rs-hdfs-report.toml",
                 help = "Configuration file path")]
     pub conf: String,
+}
+
+impl app::ArgConf for ArgConfig {
+    fn conf(&self) -> &str {
+        &self.conf
+    }
 }
 
 #[derive(Deserialize, Debug)]
@@ -16,4 +22,10 @@ pub struct Config<'a> {
     pub fluentd: fluentd::Config,
     pub hdfs: hdfs::Config,
     pub kinit: krb5::Config<'a>,
+}
+
+impl<'a> app::Conf for Config<'a> {
+    fn general(&self) -> &app::Config {
+        &self.general
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,11 @@ extern crate json_collection;
 extern crate lazy_static;
 #[macro_use]
 extern crate log;
-extern crate log4rs;
 extern crate mega_coll;
 extern crate regex;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-extern crate simple_logger;
 extern crate structopt;
 #[macro_use]
 extern crate structopt_derive;
@@ -27,19 +25,17 @@ mod conf;
 mod hdfs;
 mod krb5;
 
-use conf::{ArgConf, Config};
+use conf::{ArgConfig, Config};
 use failure::ResultExt;
 use fruently::forwardable::JsonForwardable;
 use hdfs::Hdfs;
 use krb5::Krb5;
 use mega_coll::error::{ErrorKind, Result};
-use mega_coll::error::custom::PathError;
-use mega_coll::util::app::{create_and_check_fluent, print_run_status,
-                           read_config_file};
+use mega_coll::util::app::{create_and_check_fluent, init_config,
+                           print_run_status};
 use mega_coll::util::fs::lock_file;
 use std::process;
 use std::thread;
-use structopt::StructOpt;
 
 fn run_impl(conf: &Config) -> Result<()> {
     let fluent = create_and_check_fluent(
@@ -76,24 +72,8 @@ fn run(conf: &Config) -> Result<()> {
     }
 }
 
-fn init<'a>() -> Result<Config<'a>> {
-    let arg_conf = ArgConf::from_args();
-    let conf: Config = read_config_file(&arg_conf.conf)?;
-
-    match conf.general.log_conf_path {
-        Some(ref log_conf_path) => {
-            log4rs::init_file(log_conf_path, Default::default())
-                .map_err(|e| PathError::new(log_conf_path, e))
-                .context(ErrorKind::SpecializedLoggerInit)?
-        }
-        None => simple_logger::init().context(ErrorKind::DefaultLoggerInit)?,
-    }
-
-    Ok(conf)
-}
-
 fn main() {
-    let conf_res = init();
+    let conf_res = init_config::<ArgConfig, Config>();
 
     if let Err(ref e) = conf_res {
         eprintln!("{}", e);
